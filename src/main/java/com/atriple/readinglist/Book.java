297759483592de5f3d56fc73578118bc9f282c53
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-12-15 17:25:42 +0700

    Add controller & thymeleaf web template

--- src/main/java/com/atriple/readinglist/Book.java
+++ src/main/java/com/atriple/readinglist/Book.java
@@ -55,4 +55,12 @@ public class Book {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
